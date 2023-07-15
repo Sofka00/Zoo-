6959b3cbb94335152359ec7b57535a9ceec41e8b
Питенко София <pitenko.sofiya@mail.ru>
2023-07-15 14:39:46 +0300

    Add project files.

---
+++ Zoo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animals elephant = new Animals();
+            elephant.Name = "Petya";
+            elephant.Kind = "Slon";
+            elephant.Biom = "Tropici";
+            elephant.Food = new string[1] {"Fish"};
+            elephant.Age = -5;
+            elephant.Sound = "oink-oink";
+            elephant.Quantity = 5;
+            elephant.Say();
+            elephant.Eat();
+            elephant.Play();
+            Animals penguin = new Animals();
+            penguin.Name = "Simba";
+            penguin.Kind = "Pinguin";
+            penguin.Biom = "Tundra";
+            penguin.Food = new string[2] { "Fish", "Meet" };
+            penguin.Age = 1;
+            penguin.Sound = "auf-auf";
+            penguin.Quantity = 0.3;
+            penguin.Say();
+            penguin.Eat();
+            penguin.Play();
+            Animals tiger = new Animals();
+            tiger.Name = "Matilda";
+            tiger.Kind = "Tiger";
+            tiger.Biom = "Desert";
+            tiger.Food = new string[2] { "Fish", "Meet" };
+            tiger.Age = 20;
+            tiger.Sound = "Grr-Grr";
+            tiger.Quantity = 5;
+            tiger.Say();
+            tiger.Eat();
+            tiger.Play();
+        }
+    }
+}
---
+++ Zoo/options.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    public class Animals
+    {
+        public string Kind { get; set; }
+        public string Biom { get; set; }
+        public int Area { get; set; }
+
+        public string[] Food { get; set; }
+        public string Predador { get; set; }
+        public string Sound { get; set; }
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _age = value;
+                }
+            }
+        }
+        private int _age;
+
+
+        public void Say()
+        {
+            Console.WriteLine($"{Name} {Sound} ");
+        }
+
+        public void Eat()
+        {
+            string result = "";
+            foreach (var item in Food)
+            {
+                result += " ";
+                result+= item;
+            }
+            Console.WriteLine($"{Name} Eat{result} ");
+        }
+
+        public void Play()
+        {
+            Console.WriteLine($"{Name} Play");
+        }
+        public void EatыPerDay()
+        {
+            Console.WriteLine($"{Name} eats per day {Quantity} kg."); 
+        }
+
+
+    }
+}
